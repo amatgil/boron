53d411683a8b1ba967084e0dddffebcf2690179d
amatgil <amatgilvinyes@gmail.com>
2025-01-21 21:39:31 +0100

    Initing (after cabal init)

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for boron
+
+## 0.1.0.0 -- 2025-01-21
+
+* First version. Released on an unsuspecting world.
---
+++ shell.nix
@@ -0,0 +1,24 @@
+{
+  pkgs ? import <nixpkgs> { },
+  lib,
+}:
+let
+  packages = with pkgs; [
+    haskell-language-server
+    ghc
+    ghcid # Bacon for haskell
+    cabal-install
+    haskellPackages.hindent
+    #haskellPackages.hls
+    haskellPackages.lsp
+  ];
+in
+pkgs.mkShell {
+  # Get dependencies from the main package
+  nativeBuildInputs = packages;
+  buildInputs = packages;
+  env = {
+    LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+    LD_LIBRARY_PATH = "${lib.makeLibraryPath packages}";
+  };
+}
