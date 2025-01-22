391a5380108bce1dd1b5a755ff8bcd6380bcdfc9
amatgil <amatgilvinyes@gmail.com>
2025-01-23 00:50:22 +0100

    Tweak test evaluation

--- app/Main.hs
+++ app/Main.hs
@@ -4,16 +4,20 @@ import Boron.AST
 import Boron.Eval
 import Control.Monad.State.Lazy
 
+-- evals:
+-- y := 0
+-- for x in 1..5 {
+--   y += x
+-- }
+-- y
+
 main :: IO ()
 main = let thing = [
              (Assign "y" (LiteralNum 0.0)),
              (For "x" (LiteralTuple (map LiteralNum [1, 2, 3, 4])) [
                  Reassign "y" (Call (Var "+") [Var "y", Var "x"])
-                                                  ])
+                                                  ]),
+             (Var "y")
              ]
   in print $ runState (evalExprs thing) bareEnv
         
--- y := 0
--- for x in 1..5 {
---   y += x
--- }
