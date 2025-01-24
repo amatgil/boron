81f34974d6c66265ef6abf00c5e9f332be88b6bc
amatgil <amatgilvinyes@gmail.com>
2025-01-24 19:24:21 +0100

    Added `while` and basic comparison operators!

--- app/Main.hs
+++ app/Main.hs
@@ -11,13 +11,31 @@ import Control.Monad.State.Lazy
 -- }
 -- y
 
+--main :: IO ()
+--main = let thing = [
+--             (Assign "y" (LiteralNum 0.0)),
+--             (For "x" (LiteralTuple (map LiteralNum [1, 2, 3, 4])) [
+--                 Reassign "y" (Call (Var "+") [Var "y", Var "x"])
+--                                                  ]),
+--             (Var "y")
+--             ]
+--  in print $ runState (evalExprs thing) bareEnv
+        
+
+-- evals:
+-- y := 10
+-- while >(y, 6) {
+--   y = -(y, 1)
+-- }
+-- y
+
 main :: IO ()
 main = let thing = [
-             (Assign "y" (LiteralNum 0.0)),
-             (For "x" (LiteralTuple (map LiteralNum [1, 2, 3, 4])) [
-                 Reassign "y" (Call (Var "+") [Var "y", Var "x"])
-                                                  ]),
-             (Var "y")
+             Assign "y" (LiteralNum 10.0),
+             While (Call (Var ">") [Var "y", LiteralNum 6.0]) [
+                 Reassign "y" (Call (Var "-") [Var "y", LiteralNum 1.0])
+                 ],
+             Var "y"
              ]
   in print $ runState (evalExprs thing) bareEnv
         
